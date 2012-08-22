57952a587f155987cc26177fcd46b8e524779fcb
Kawamoto Yuki <kawamoto62@gmail.com>
2012-08-22 10:29:06 +0900

    first commit
    rails new Cloudinary_sample -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudinarySample::Application.config.secret_token = '13040d2a4eee16b84297373bbedc56601e492f00b61a2e7e9a101153ee4e617d5b0b30fdc6281caeef9aee7e5e7741ffceadc2292b64ac649a21c8d9ac42f61b'
