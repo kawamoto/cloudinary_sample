12458b672d20f2d77d47df44583a5e3037a81d2b
Kawamoto Yuki <kawamoto62@gmail.com>
2012-08-22 12:28:44 +0900

    modify picture_uploader for cloudinary
    http://cloudinary.com/documentation/rails_integration

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,15 +10,32 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::RailsHelper
   # include Sprockets::Helpers::IsolatedHelper
 
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  process :tags => ['post_pictures']
+
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+  
+  version :thumbnail do
+    process :resize_to_fit => [50, 50]
+  end
+
+  def public_id
+    return model.short_name
+  end  
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
